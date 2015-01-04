b83a4725b1af6048fd256381c6b90178fea4d745
Pawel Miech <pawelmhm@gmail.com>
2015-01-04 14:10:56 +0100

    adds mobile user agent to get properly styled content

--- url2epub/helpers.py
+++ url2epub/helpers.py
@@ -3,7 +3,9 @@ from twisted.python import log
 
 def request_get(url, load_json=False):
     log.msg("sending request to url %s" % url)
-    dfd = get(url)
+    headers = {
+            "user-agent": "Mozilla/5.0 (Linux; U; Android 2.2; en-us; SCH-I800 Build/FROYO) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"}
+    dfd = get(url, headers=headers)
 
     def done(response):
         if load_json:
